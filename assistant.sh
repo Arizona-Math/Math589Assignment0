b60c6b764c1c793830f00dc67564ac1d44674ffc
Marek Rychlik <rychlik@arizona.edu>
2025-08-17 10:58:17 -0700

    *** empty log message ***

--- assistant.sh
+++ assistant.sh
@@ -5,6 +5,9 @@ LOCAL='./A/Assignment0'
 GITHUB_TOKEN=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
 OPENAI_API_KEY=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
 
+export GITHUB_TOKEN
+export OPENAI_API_KEY
+
 python repo_007.py \
   --repo https://github.com/${OWNER}/${REPO}.git \
   --task "Review code and propose patches" \
