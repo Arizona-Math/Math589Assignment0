215f55790250f32f9a37ade1b674fa966015832e
Marek Rychlik <rychlik@arizona.edu>
2025-08-17 11:14:09 -0700

    *** empty log message ***

--- assistant.sh
+++ assistant.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 OWNER='Arizona-Math'
 REPO='Math589Assignment0'
-LOCAL='./A/Assignment0'
+LOCAL='./Math589Assignment0'
 GITHUB_TOKEN=`cat $HOME/Domains/GitHub/ForOpenAI.txt`
 OPENAI_API_KEY=`cat $HOME/Domains/OpenAI/GeneralKey.txt`
 
